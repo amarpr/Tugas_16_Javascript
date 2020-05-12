41b6945e8b59cfc471b2128cdc4ca0ff3b8c2c40
Muhamar <amararrasyid@gmail.com>
2020-05-12 17:53:21 +0700

    Tugas 16 Javascript
    Mengumpulkan Tugas Javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+function min() {
+    var angka = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19);
+    console.log("Nilai Minimalnya adalah : " + angka)
+}
+
+
+function max() {
+    var angka = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+    console.log("Nilai Maximalnya adalah : " + angka)
+}
+
+min()
+max()
